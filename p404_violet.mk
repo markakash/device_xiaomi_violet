7c4a71148e782d969e4a2557c0f87dd98492bfed
Akash <akashniki@gmail.com>
2020-12-24 19:59:58 +0100

    violet: Build with gapps

--- p404_violet.mk
+++ p404_violet.mk
@@ -12,6 +12,11 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 $(call inherit-product, vendor/404/configs/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
+TARGET_GAPPS_ARCH := arm64
+
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := p404_violet
 PRODUCT_DEVICE := violet
